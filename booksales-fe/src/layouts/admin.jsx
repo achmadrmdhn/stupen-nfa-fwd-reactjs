7c6e60e70eed117c6dfad95faf2ca720b1d291f5
achmadrmdhn <achmadrifai0511@gmail.com>
2025-10-30 11:57:06 +0700

    tugas 3 - react lanjutan

--- booksales-fe/src/layouts/admin.jsx
+++ booksales-fe/src/layouts/admin.jsx
@@ -1,6 +1,32 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import { logout, useDecodeToken } from "../_services/auth";
+import { useEffect } from "react";
 
 export default function Adminlayout() {
+  const navigate = useNavigate();
+  const token = localStorage.getItem("accessToken");
+  const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const decodedData = useDecodeToken(token);
+
+  useEffect(() => {
+    if (!token || !decodedData || !decodedData.success) {
+      navigate("/login");
+    }
+
+    const role = userInfo.role;
+    if (role !== "admin" || !role) {
+      navigate("/");
+    }
+  }, [token, decodedData, navigate]);
+
+  const handleLogout = async () => {
+    if (token) {
+      await logout({ token });
+      localStorage.removeItem("userInfo")
+      navigate("/login");
+    }
+  };
+
   return (
     <>
       <div className="antialiased bg-gray-50 dark:bg-gray-900">
@@ -78,6 +104,12 @@ export default function Adminlayout() {
                 </svg>
               </button>
 
+              <Link
+                to={"/"}
+                className="bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+              >
+                {userInfo.name}
+              </Link>
               <button
                 type="button"
                 className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
@@ -85,7 +117,6 @@ export default function Adminlayout() {
                 aria-expanded="false"
                 data-dropdown-toggle="dropdown"
               >
-                <span className="sr-only">Open user menu</span>
                 <img
                   className="w-8 h-8 rounded-full"
                   src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/michael-gough.png"
@@ -282,6 +313,15 @@ export default function Adminlayout() {
                   </svg>
                   <span className="ml-3">Help</span>
                 </Link>
+              </li>
+
+              <li>
+                <button
+                  onClick={handleLogout}
+                  className="flex items-center p-2 text-base font-medium text-gray-900 rounded-lg transition duration-75 bg-red-100 hover:bg-red-200 dark:hover:bg-red-700 dark:text-white group"
+                >
+                  <span className="ml-3">Logout</span>
+                </button>
               </li>
             </ul>
           </div>
